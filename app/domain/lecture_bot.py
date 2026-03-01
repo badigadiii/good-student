edd9779b0a0ff5797776700f5d2c41bce4f2b109
badigadiii <bigtinyturtle@gmail.com>
2026-03-01 20:12:52 +0400

    add steps logging

--- app/domain/lecture_bot.py
+++ app/domain/lecture_bot.py
@@ -1,3 +1,4 @@
+import logging
 import string
 from collections.abc import Callable, Sequence
 from datetime import datetime
@@ -5,6 +6,8 @@ from datetime import datetime
 from app.core.config import BotSettings
 from app.domain.interfaces import LectureClient
 from app.domain.models import ChatMessage, LectureConfig
+
+logger = logging.getLogger(__name__)
 
 
 class LectureBot:
@@ -37,27 +40,55 @@ class LectureBot:
 
     async def run(self, now_provider: Callable[..., datetime]) -> None:
         try:
+            logger.info("Lecture bot run started for student=%s", self._config.student_name)
             if self._config.lecture_start:
+                logger.debug(
+                    "Waiting for lecture start at %s",
+                    self._config.lecture_start.isoformat(),
+                )
                 while True:
-                    if self._current_time(now_provider) >= self._config.lecture_start:
+                    current_time = self._current_time(now_provider)
+                    if current_time >= self._config.lecture_start:
+                        logger.info("Lecture start time reached at %s", current_time.isoformat())
                         break
+                    logger.debug(
+                        "Lecture has not started yet, current_time=%s",
+                        current_time.isoformat(),
+                    )
                     await self._sleep(self._settings.lecture_start_poll_interval_ms)
 
+            logger.info("Joining lecture at %s", self._config.lecture_url)
             await self._client.join(self._config)
             await self._sleep(self._settings.post_join_delay_ms)
+            logger.info("Sending greeting message")
             await self._client.send_message(self._config.greetings_message)
 
             while True:
                 messages = await self._client.get_messages()
-                if self.should_finish(messages, self._current_time(now_provider)):
+                current_time = self._current_time(now_provider)
+                logger.debug(
+                    "Polled chat messages at %s: %s",
+                    current_time.isoformat(),
+                    messages,
+                )
+                if self.is_lecture_over_by_keyphrases(messages):
+                    logger.info("Lecture finish detected by keyphrases")
+                    break
+                if self.is_lecture_over_by_time(current_time):
+                    logger.info(
+                        "Lecture finish detected by schedule at %s", current_time.isoformat()
+                    )
                     break
                 await self._sleep(self._settings.chat_poll_interval_ms)
 
             await self._sleep(self._settings.pre_goodbye_delay_ms)
+            logger.info("Sending goodbye message")
             await self._client.send_message(self._config.goodbye_message)
             await self._sleep(self._settings.pre_leave_delay_ms)
+            logger.info("Leaving lecture")
             await self._client.leave()
         finally:
+            logger.debug("Closing lecture client")
             await self._client.close()
 
     def _current_time(self, now_provider: Callable[..., datetime]) -> datetime:
